c7b442b77ce8c2c8f53cd7cde0a2cf5bc9a7b0ec
Dylan Schoop <42898108+CurleyDS@users.noreply.github.com>
2023-05-09 16:09:48 +0200

    Les 1 - Week 1 Ochtend

--- src/js/game.js
+++ src/js/game.js
@@ -11,11 +11,40 @@ export class Game extends Engine {
 
     startGame() {
         console.log("start de game!")
-        const fish = new Actor()
+        const background = new Actor()
+        background.graphics.use(Resources.Background.toSprite())
+        background.pos = new Vector(400, 300)
+        this.add(background)
+
+        const fish = new Actor({
+            width: Resources.Fish.width,
+            height: Resources.Fish.height
+        })
         fish.graphics.use(Resources.Fish.toSprite())
         fish.pos = new Vector(400, 300)
-        fish.vel = new Vector(-10,0)
+        fish.vel = new Vector(-100,0)
+        fish.on("pointerup", function (event) {
+            fish.graphics.use(Resources.Bones.toSprite())
+        })
+        fish.on("exitviewport", (event) => {
+            fish.pos = new Vector(900, Math.random()*300 + 200)
+        })
         this.add(fish)
+
+        const shark = new Actor({
+            width: Resources.Shark.width,
+            height: Resources.Shark.height
+        })
+        shark.graphics.use(Resources.Shark.toSprite())
+        shark.pos = new Vector(200, 150)
+        shark.vel = new Vector(-100,0)
+        shark.on("pointerup", function (event) {
+            shark.graphics.use(Resources.Bones.toSprite())
+        })
+        shark.on("exitviewport", (event) => {
+            shark.pos = new Vector(900, Math.random()*300 + 200)
+        })
+        this.add(shark)
     }
 }
 
--- src/js/resources.js
+++ src/js/resources.js
@@ -1,9 +1,20 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
+import bgImage from '../images/water.jpg'
 import fishImage from '../images/fish.png'
+import sharkImage from '../images/shark.png'
+import bonesImage from '../images/bones.png'
 
 const Resources = {
-    Fish: new ImageSource(fishImage)
+    Background: new ImageSource(bgImage),
+    Fish: new ImageSource(fishImage),
+    Shark: new ImageSource(sharkImage),
+    Bones: new ImageSource(bonesImage)
 }
-const ResourceLoader = new Loader([Resources.Fish])
+const ResourceLoader = new Loader([
+    Resources.Background,
+    Resources.Fish,
+    Resources.Shark,
+    Resources.Bones
+])
 
 export { Resources, ResourceLoader }
